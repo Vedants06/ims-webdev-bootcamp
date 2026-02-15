51dc393347fcb25f9f2f36154c6514f4c874e6db
AtharvaGGourshete <goursheteatharva@gmail.com>
2026-02-15 22:00:49 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-[#141414] text-white">
+      <h1 className="text-center text-6xl">
+        Inventory Management System
+      </h1>
+    </div>
+  );
+}
+
+export default App;
